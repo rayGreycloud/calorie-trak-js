dba29dcd50528e420da9fac840c36b237ff818bd
rayGreycloud <rayGreycloud@gmail.com>
2018-02-19 09:39:59 -0800

    Add update list item method to UICtrl

--- ui-controller.js
+++ ui-controller.js
@@ -2,6 +2,7 @@
 const UICtrl = (() => {
   const UISelectors = {
     itemList: '#item-list',
+    listItems: '#item-list li',
     addBtn: '.add-btn',
     backBtn: '.back-btn',
     updateBtn: '.update-btn',
@@ -30,12 +31,6 @@ const UICtrl = (() => {
       // Insert list items into DOM
       document.querySelector(UISelectors.itemList).innerHTML = html; 
     },
-    getItemInput: () => {
-      return {
-        name: document.querySelector(UISelectors.itemNameInput).value,
-        calories: document.querySelector(UISelectors.itemCaloriesInput).value
-      }
-    },
     addListItem: (item) => {
       // Show the list 
       document.querySelector(UISelectors.itemList).style.display = 'block';      
@@ -52,8 +47,36 @@ const UICtrl = (() => {
           <i class="edit-item fa fa-pencil fa-lg"></i>
         </a>
       `;
-      // Insert item 
+      // Insert item into UI
       document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);    
+    },
+    updateListItem: (updatedItem) => {
+      // Get items from DOM
+      let listItems = document.querySelectorAll(UISelectors.listItems);
+      // Convert node list into array (to facilitate iteration)
+      listItems = Array.from(listItems);
+      // Iterate over items 
+      listItems.forEach(listItem => {
+        // Get item id
+        const listItemId = listItem.getAttribute('id');
+        // Check if item id matches update item id 
+        if (listItemId === `item-${updatedItem.id}`) {
+          // Inject updated item into li 
+          document.querySelector(`#${listItemId}`).innerHTML = `
+            <strong>${updatedItem.name}</strong> - <em>${updatedItem.calories} calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil fa-lg"></i>
+            </a>
+          `;
+        }
+      });
+    },
+    getSelectors: () => UISelectors,    
+    getItemInput: () => {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value
+      }
     },
     clearInput: () => {
       document.querySelector(UISelectors.itemNameInput).value = '';
@@ -82,7 +105,6 @@ const UICtrl = (() => {
       document.querySelector(UISelectors.deleteBtn).style.display = 'inline';      
       document.querySelector(UISelectors.updateBtn).style.display = 'inline';      
       document.querySelector(UISelectors.backBtn).style.display = 'inline';      
-    },
-    getSelectors: () => UISelectors
+    }
   }
 })();
